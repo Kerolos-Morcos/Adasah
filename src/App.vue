bfcd5fd75b36a997a0aa65a57f8ca999362bb5d4
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2026-01-30 00:42:25 +0200

    app component edit

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,6 @@
 import Footer from './components/Footer.vue';
 import Navbar from './components/Navbar.vue';
 
-// fetch("./store/posts.json")
-//   .then(res => res.json())
-//   .then(data => {
-//     console.log(data.posts);
-//   });
-
 </script>
 
 <template>
